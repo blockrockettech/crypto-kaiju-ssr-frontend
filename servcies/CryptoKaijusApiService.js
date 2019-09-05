435bf9f47cb9cb433e3782b1f35d850019798369
James Morgan <james.morgan.e@googlemail.com>
2019-09-05 09:37:10 +0100

    Fixing CORS issue

--- servcies/CryptoKaijusApiService.js
+++ servcies/CryptoKaijusApiService.js
@@ -9,6 +9,24 @@ const API_CONFIG = {
     live: 'https://api.cryptokaiju.io/api'
 };
 
+const getApi = () => {
+    if (process.browser) {
+        switch (window.location.hostname) {
+            case '0.0.0.0':
+            case 'localhost':
+            case '127.0.0.1':
+                return API_CONFIG.local;
+            default:
+                return API_CONFIG.live;
+        }
+    } else {
+        if (process.env.NODE_ENV === 'development') {
+            return API_CONFIG.local;
+        }
+        return API_CONFIG.live;
+    }
+};
+
 const AXIOS_CONFIG = {
     headers: {
         'Access-Control-Allow-Origin': '*'
@@ -18,57 +36,57 @@ const AXIOS_CONFIG = {
 class CryptoKaijusApiService {
 
     async getTokenDetails(network = 1, tokenId) {
-        return axios.get(`${API_CONFIG.live}/network/${network}/token/id/${tokenId}`, AXIOS_CONFIG)
+        return axios.get(`${getApi()}/network/${network}/token/id/${tokenId}`, AXIOS_CONFIG)
             .then((payload) => payload.data);
     }
 
     async getNfcDetails(network = 1, nfcId) {
-        return axios.get(`${API_CONFIG.live}/network/${network}/token/nfc/${nfcId}`, AXIOS_CONFIG)
+        return axios.get(`${getApi()}/network/${network}/token/nfc/${nfcId}`, AXIOS_CONFIG)
             .then((payload) => payload.data);
     }
 
     async getTokensForAddress(network = 1, address) {
-        return axios.get(`${API_CONFIG.live}/network/${network}/token/account/${address}`, AXIOS_CONFIG)
+        return axios.get(`${getApi()}/network/${network}/token/account/${address}`, AXIOS_CONFIG)
             .then((payload) => payload.data);
     }
 
     async getTokenTraits(network = 1) {
-        return axios.get(`${API_CONFIG.live}/network/${network}/token/traits`, AXIOS_CONFIG)
+        return axios.get(`${getApi()}/network/${network}/token/traits`, AXIOS_CONFIG)
             .then((payload) => payload.data);
     }
 
     async getAllTokens(network = 1) {
-        return axios.get(`${API_CONFIG.live}/network/${network}/token/all`, AXIOS_CONFIG)
+        return axios.get(`${getApi()}/network/${network}/token/all`, AXIOS_CONFIG)
             .then((payload) => payload.data);
     }
 
     async getHomePageDetails(network = 1) {
-        return axios.get(`${API_CONFIG.live}/network/${network}/homepage`, AXIOS_CONFIG)
+        return axios.get(`${getApi()}/network/${network}/homepage`, AXIOS_CONFIG)
             .then((payload) => payload.data);
     }
 
     async searchByTokenId(network = 1, tokenId) {
-        return axios.get(`${API_CONFIG.live}/network/${network}/search/id/${tokenId}`, AXIOS_CONFIG)
+        return axios.get(`${getApi()}/network/${network}/search/id/${tokenId}`, AXIOS_CONFIG)
             .then((payload) => payload.data);
     }
 
     async searchByNfcId(network = 1, nfcId) {
-        return axios.get(`${API_CONFIG.live}/network/${network}/search/nfc/${nfcId}`, AXIOS_CONFIG)
+        return axios.get(`${getApi()}/network/${network}/search/nfc/${nfcId}`, AXIOS_CONFIG)
             .then((payload) => payload.data);
     }
 
     async getOpenSeaDetials(network = 1, tokenId) {
-        return axios.get(`${API_CONFIG.live}/network/${network}/os/id/${tokenId}`, AXIOS_CONFIG)
+        return axios.get(`${getApi()}/network/${network}/os/id/${tokenId}`, AXIOS_CONFIG)
             .then((payload) => payload.data);
     }
 
     async findKittyDataById(kittyId) {
-        return axios.get(`${API_CONFIG.live}/network/1/os/search/kitty-data/${kittyId}`, AXIOS_CONFIG)
+        return axios.get(`${getApi()}/network/1/os/search/kitty-data/${kittyId}`, AXIOS_CONFIG)
             .then((payload) => payload.data);
     }
 
     async buildBandanaSvg(kittyId) {
-        return axios.get(`${API_CONFIG.live}/image/decorate/kitty/${kittyId}/bandana`, AXIOS_CONFIG)
+        return axios.get(`${getApi()}/image/decorate/kitty/${kittyId}/bandana`, AXIOS_CONFIG)
             .then((payload) => payload.data);
     }
 }
